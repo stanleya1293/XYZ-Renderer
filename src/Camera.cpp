7bf6846c624a7a73cc53fd777bdee6d56eb2365f
stanleya1293 <arden.stanley@yahoo.com>
2025-01-28 15:24:12 -0500

    Camera system work

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,7 +1,12 @@
 #include "Camera.h"
 
-Camera::Camera()
+Camera::Camera() :
+	m_position(),
+	m_direction(),
+	m_view()
 {
+	m_direction = glm::vec3();
+	m_view = glm::lookAt(m_position, m_position + glm::normalize(m_direction), glm::vec3(0.0f, 1.0f, 0.0f));
 
 }
 
--- src/Camera.h
+++ src/Camera.h
@@ -1,13 +1,19 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 class Camera
 {
 public:
 	Camera();
 	~Camera();
 private:
-
+	glm::vec3 m_position;
+	glm::vec3 m_direction;
+	glm::mat4 m_view;
 };
 
 #endif
