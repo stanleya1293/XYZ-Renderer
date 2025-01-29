2b25a5b87c8a31808ce0b3f046ffd8ac8eed43c5
Ardeniscool1293 <arden.stanley@yahoo.com>
2025-01-28 22:53:35 -0500

    Camera update

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,18 +1,19 @@
 #include "Camera.h"
 
-Camera::Camera() :
+Camera::Camera(Shader shader) :
 	m_yaw(-90.0f),
 	m_pitch(0.0f),
 	m_direction(0),
 	m_position(0),
-	m_view(0)
+	m_view(0),
+	m_shader(shader)
 {
 	m_direction.x = (1 - sin(glm::radians(m_yaw))) * (1 - sin(glm::radians(m_pitch)));
 	m_direction.y = sin(glm::radians(m_pitch));
 	m_direction.z = (sin(glm::radians(m_yaw))) * (1 - sin(glm::radians(m_pitch)));
 
 	m_view = glm::lookAt(m_position, m_position + glm::normalize(m_direction), glm::vec3(0.0f, 1.0f, 0.0f));
-
+	
 }
 
 Camera::~Camera()
@@ -20,7 +21,10 @@ Camera::~Camera()
 
 }
 
-void Camera::update()
+void Camera::move(float xOffset, float yOffset, float zOffset)
 {
-	
+	m_position.x += xOffset;
+	m_position.y += yOffset;
+	m_position.z += zOffset;
+
 }
--- src/Camera.h
+++ src/Camera.h
@@ -5,18 +5,21 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "Shader.h"
+
 class Camera
 {
 public:
-	Camera();
+	Camera(Shader shader);
 	~Camera();
-	void update();
+	void move(float xOffset, float yOffset, float zOffset);
 private:
 	float m_yaw;
 	float m_pitch;
 	glm::vec3 m_direction;
 	glm::vec3 m_position;
 	glm::mat4 m_view;
+	Shader m_shader;
 };
 
 #endif
