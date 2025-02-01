b8c9fd8560b79b14c821fa6dda14c4e2d29e6515
stanleya1293 <arden.stanley@yahoo.com>
2025-01-31 19:35:39 -0500

    working on camera movement

--- src/Application.cpp
+++ src/Application.cpp
@@ -15,20 +15,54 @@ void Application::run()
 {
 	std::vector<Vertex> vertices =
 	{
-		Vertex {{ -0.5f,  -0.5f,  0.0f  }},
-		Vertex {{ -0.5f,   0.5f,  0.0f  }},
-		Vertex {{  0.5f,  -0.5f,  0.0f  }},
+    Vertex{{-0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f,  0.5f, -0.5f}},
+    Vertex{{ 0.5f,  0.5f, -0.5f}},
+    Vertex{{-0.5f,  0.5f, -0.5f}},
+    Vertex{{-0.5f, -0.5f, -0.5f}},
 
-		Vertex {{  0.5f,  -0.5f,  0.0f  }},
-		Vertex {{ -0.5f,   0.5f,  0.0f  }},
-		Vertex {{  0.5f,   0.5f,  0.0f  }}
+    Vertex{{-0.5f, -0.5f,  0.5f}},
+    Vertex{{ 0.5f, -0.5f,  0.5f}},
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+    Vertex{{-0.5f,  0.5f,  0.5f}},
+    Vertex{{-0.5f, -0.5f,  0.5f}},
+
+    Vertex{{-0.5f,  0.5f,  0.5f}},
+    Vertex{{-0.5f,  0.5f, -0.5f}},
+    Vertex{{-0.5f, -0.5f, -0.5f}},
+    Vertex{{-0.5f, -0.5f, -0.5f}},
+    Vertex{{-0.5f, -0.5f,  0.5f}},
+    Vertex{{-0.5f,  0.5f,  0.5f}},
+
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+    Vertex{{ 0.5f,  0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f,  0.5f}},
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+
+    Vertex{{-0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f, -0.5f}},
+    Vertex{{ 0.5f, -0.5f,  0.5f}},
+    Vertex{{ 0.5f, -0.5f,  0.5f}},
+    Vertex{{-0.5f, -0.5f,  0.5f}},
+    Vertex{{-0.5f, -0.5f, -0.5f}},
+
+    Vertex{{-0.5f,  0.5f, -0.5f}},
+    Vertex{{ 0.5f,  0.5f, -0.5f}},
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+    Vertex{{ 0.5f,  0.5f,  0.5f}},
+    Vertex{{-0.5f,  0.5f,  0.5f}},
+    Vertex{{-0.5f,  0.5f, -0.5f}},    
 	};
 
 	Model square(vertices, {0.0f, 0.0f, 0.0f}, 1.0f);
 
 	Shader def("default");
 
-	//Camera camera(def);
+	Camera camera(def);
 
 	while (m_window.running())
 	{
